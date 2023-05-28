32a392dc26618a3b040003579cddaf857276c229
KARTIKE KISHORE <56794843+kartikekishore@users.noreply.github.com>
2023-05-28 14:39:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
-var cityVar;
-var p1 = "https://api.weatherbit.io/v2.0/current?&city=";
-var p2 = "&key=0bf451f91ff64705bc45ce2a4c766587&include=minutely";
-const settings = {
-  async: true,
-  crossDomain: true,
-  url: cityVar,
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
-    "x-rapidapi-key": "bf639f53admshf988df62756c1bcp1279a6jsn1e08bd79566b",
-  },
-};
 
-$("#search_btn").on("click", function (event) {
-  console.log($("#searchbox").val());
-  cityVar = p1 + $("#searchbox").val() + p2;
+const express=require("express");
+const bodyparser=require("body-parser");
+const axios = require('axios');
+const PORT=3000;
 
-  $.ajax(cityVar).done(function (response) {
-    console.log(response);
-    $(".wind-speed").html(response.data[0].wind_spd + " m/s");
-    $(".pressure").html(response.data[0].pres + "mb");
-    $(".current-temp").html(response.data[0].temp + "&deg;C");
-    $(".location").html(
-      response.data[0].city_name + ", " + response.data[0].country_code
-    );
-    $(".weather-desc").html(response.data[0].weather.description);
-    $(".uvi").html(response.data[0].uv + "UVI");
-    if(response.data[0].uv<=5)
-    {
-        $(".uvi_desc").html("Low risk of UV rays");
-    }
-    else{
-        $(".uvi_desc").html("Moderate risk of UV rays");
-    }
-    $(".prec").html( response.data[0].precip+ "%");
-    $(".wind-deg").html( response.data[0].wind_dir+ "&deg; wind");
+app=express();
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{  
     
-  });
+    // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid={b862f25e592d18228944b5f049c65d99}
+    res.sendFile(__dirname+'/index.html');
+})
+
+app.post("/",async(req,res)=>{
+
+    let city=req.body.city;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b862f25e592d18228944b5f049c65d99`;
+
+    const response = await axios.get(url);
+    console.log(response);
+     
+      
+ 
+    
+
+    res.send(`<h1>Temperature in ${city} is `+response.data.main.temp +  "<br>Feels Like "  + response.data.main.feels_like +"<h1>");
+})
+
+app.listen(PORT ,()=>{
+    console.log(`listening on port ${PORT} `);
 });
